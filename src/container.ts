0957656d019297ed720bc8ff2ccf7648ef772a57
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-12-01 16:25:16 -0300

    feat: add injections dependencies

--- src/container.ts
+++ src/container.ts
@@ -1,1 +1,17 @@
-// Injection of dependencies
+import { PrismaClient } from '@prisma/client'
+import { AuthWithGoogleController } from './controllers/auth/google'
+import { UserPrismaRepository } from './repositories/prisma/user-prisma.repository'
+import { AuthWithGoogleService } from './services/auth/google'
+
+const prisma = new PrismaClient()
+
+// Repositories
+const userRepository = new UserPrismaRepository(prisma)
+
+// Services
+const authGoogleService = new AuthWithGoogleService(userRepository)
+
+// Controllers
+export const authGoogleController = new AuthWithGoogleController(
+  authGoogleService
+)

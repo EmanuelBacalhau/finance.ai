080447763400fa5d4709bc67d6972cb130c1ee29
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-12-01 16:24:28 -0300

    feat: add environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config'
+
 import { z } from 'zod'
 
 const EnvSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  BASE_URL: z.string(),
+  JWT_SECRET: z.string(),
 })
 
 const { success, data, error } = EnvSchema.safeParse(process.env)
